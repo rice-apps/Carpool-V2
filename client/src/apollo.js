021c4e1403f9a0f209aaec532a719c9335ccbb07
Will Mundy <TheWillMundy@users.noreply.github.com>
2020-05-24 04:14:13 -0400

    Removed subscriptions for now since we do not need them.

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,15 +1,9 @@
 // Apollo Client Setup
-import { ApolloClient, HttpLink, InMemoryCache, split, gql } from '@apollo/client';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
 import { setContext } from '@apollo/link-context';
 
 // Apollo Subscriptions Setup
-import { WebSocketLink } from '@apollo/link-ws';
 import { GRAPHQL_URL, GRAPHQL_WS_URL, SERVICE_URL } from './config';
-
-console.log(GRAPHQL_URL);
-console.log(GRAPHQL_WS_URL);
-console.log(SERVICE_URL);
 
 // Wraps our requests with a token if one exists
 // Copied from: https://www.apollographql.com/docs/react/v3.0-beta/networking/authentication/
@@ -30,34 +24,13 @@ const httpLink = new HttpLink({
     uri: GRAPHQL_URL
 });
 
-// WebSocket Backend Link
-const wsLink = new WebSocketLink({
-    uri: GRAPHQL_WS_URL,
-    options: {
-        reconnect: true
-    }
-});
-
-// Uses wsLink for subscriptions, httpLink for queries & mutations (everything else)
-const splitLink = split(
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
-);
-
 // Setup cache
 const cache = new InMemoryCache();
 
 // Initialize Client
 export const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(splitLink),
+    link: authLink.concat(httpLink),
 });
 
 // Initial local state
