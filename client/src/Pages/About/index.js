c244194ec0bcbb958b2c5d355d61c35390d46a72
willie-yao <william2000yao@gmail.com>
2020-08-13 12:47:38 -0700

    Cleaned up faq code

--- client/src/Pages/About/index.js
+++ client/src/Pages/About/index.js
@@ -147,29 +147,28 @@ const About = ({}) => {
                 <DetailText>
                     Frequently Asked Questions:
                 </DetailText>
-                {/* <Accordion title="Who can use Carpool?">
+                <SimpleAccordion title="Who can use Carpool?">
                     Carpool was created for the Rice University Community. A Net ID is required.
-                </Accordion>
-                <Accordion title="Do I need to download Carpool on my phone?">
+                </SimpleAccordion>
+                <SimpleAccordion title="Do I need to download Carpool on my phone?">
                     No, Carpool is not a mobile device app. It was created as website to be accessible through all devices including desktop computers.
-                </Accordion>
-                <Accordion title="How do I sign up for Carpool?">
+                </SimpleAccordion>
+                <SimpleAccordion title="How do I sign up for Carpool?">
                     No sign up is necessary! All you need is your Rice Net ID to log onto the site: carpool.riceapps.org
-                </Accordion>
-                <Accordion title="I’ve signed in for the first time, now what?">
+                </SimpleAccordion>
+                <SimpleAccordion title="I’ve signed in for the first time, now what?">
                     Fill out your profile page with your first name, last name, and phone number.
                     Now you can join and create rides!
-                </Accordion>
-                <Accordion title="How do I see my past and future rides?">
+                </SimpleAccordion>
+                <SimpleAccordion title="How do I see my past and future rides?">
                     You can find the rides you’ve taken in the past and the rides you’ve scheduled listed on your profile.
-                </Accordion>
-                <Accordion title="If I leave a ride, will the ride disappear?">
+                </SimpleAccordion>
+                <SimpleAccordion title="If I leave a ride, will the ride disappear?">
                     If you created a ride and were the only one on the ride, the ride will be deleted. If you joined a ride with other existing riders, the ride will exist but you will not be listed as a rider.
-                </Accordion>
-                <Accordion title="Is my information shared with outside parties?">
+                </SimpleAccordion>
+                <SimpleAccordion title="Is my information shared with outside parties?">
                     No, your information is not shared outside the application. It is only used for coordinating rides with your fellow Rice Owls.
-                </Accordion> */}
-                <SimpleAccordion/>
+                </SimpleAccordion>
             </TextDiv>
             <PictureDiv>
                 <PictureText>
